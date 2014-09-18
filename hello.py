690b692559b32d0d68db9fc952d442cf8b0ca2df
Rob Jones <robert.buckingham.jones@gmail.com>
2014-09-18 17:33:41 +0100

    take and show text

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+	if request.method == 'POST':
+		text = request.form['text']
+		return render_template('output.html', text=text)
+	else:
+		return render_template('input.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
