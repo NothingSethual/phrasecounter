3aeccdd78e7039e3b309d1c3f3dddd718b67aebd
Rob Jones <robert.buckingham.jones@gmail.com>
2014-09-19 16:38:22 +0200

    barely working cloud

--- hello.py
+++ hello.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request
-import re, string
+import re
+import string
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def app_page():
     if request.method == 'POST':
-        entries = text_to_counted_phrases(request.form['text'], 
+        entries = text_to_counted_phrases(
+            request.form['text'],
             int(request.form['num_words']))
         return render_template('output.html', entries=entries)
     else:
         return render_template('input.html')
+
 
 def remove_punctuation(s):
     regex1 = re.compile('[%s]' % re.escape(string.punctuation))
@@ -17,20 +21,25 @@ def remove_punctuation(s):
     s2 = regex1.sub('', s)
     return regex2.sub(' ', s2)
 
+
 def clean(s):
     return remove_punctuation(s).casefold()
 
+
 def list_phrases(words, num):
-    return set(" ".join(words[i:i+num]) for i in range(len(words)) \
-        if len(words[i:i+num]) == num)
+    return set(" ".join(words[i:i+num]) for i in range(len(words))
+               if len(words[i:i+num]) == num)
+
 
 def count_phrases(phrases, text):
     return set(map(lambda p: (text.count(p), p), phrases))
+
 
 def text_to_counted_phrases(text, num_words):
     words = clean(text).split()
     phrases = list_phrases(words, num_words)
     return sorted(count_phrases(phrases, " ".join(words)), reverse=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
